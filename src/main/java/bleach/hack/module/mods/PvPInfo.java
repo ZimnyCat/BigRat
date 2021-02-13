9dba3ac4fe4eab1233a59bd6e8f8c70410296162
ZimnyCat <thezimnycat@gmail.com>
2021-02-13 10:18:59 +0500

    добавлено кол-во попнутых тотемов в PvPInfo

--- src/main/java/bleach/hack/module/mods/PvPInfo.java
+++ src/main/java/bleach/hack/module/mods/PvPInfo.java
@@ -3,6 +3,7 @@ package bleach.hack.module.mods;
 import bleach.hack.event.events.EventDrawOverlay;
 import bleach.hack.module.Category;
 import bleach.hack.module.Module;
+import bleach.hack.module.ModuleManager;
 import bleach.hack.setting.base.SettingSlider;
 import bleach.hack.setting.base.SettingToggle;
 import com.google.common.eventbus.Subscribe;
@@ -25,12 +26,14 @@ public class PvPInfo extends Module {
                 new SettingSlider("Width", 1, 1000, 150, 1), // 2
                 new SettingToggle("HP", true), // 3
                 new SettingToggle("Ping", true), // 4
-                new SettingToggle("Distance", true)); // 5
+                new SettingToggle("Distance", true), // 5
+                new SettingToggle("Popped", true)); // 6
     }
 
     @Subscribe
     public void onDraw(EventDrawOverlay e) {
         List<AbstractClientPlayerEntity> players = new ArrayList<>();
+        TotemPopCounter tpc = new TotemPopCounter();
         for (Entity p : mc.world.getPlayers().stream().sorted(Comparator.comparingDouble(a -> mc.player.getPos().distanceTo(a.getPos()))).collect(Collectors.toList())) {
             if (p == mc.player || mc.player.distanceTo(p) > getSetting(0).asSlider().getValue()) continue;
             players.add((AbstractClientPlayerEntity) p);
@@ -41,10 +44,13 @@ public class PvPInfo extends Module {
             try { ping = mc.player.networkHandler.getPlayerListEntry(p.getUuid()).getLatency(); }
             catch (Exception exception) { ping = -1; }
             List<String> info = new ArrayList<>();
-            info.add("\u00a73" + p.getDisplayName().getString());
+            String name = p.getDisplayName().getString();
+            info.add("\u00a73" + name);
             if (getSetting(3).asToggle().state) info.add(" \u00a7fHP [\u00a73" + Math.round(p.getHealth()) + "\u00a7f]");
             if (getSetting(4).asToggle().state) info.add(" \u00a7fPing [\u00a73" + ping + "\u00a7f]");
             if (getSetting(5).asToggle().state) info.add(" \u00a7fDistance [\u00a73" + Math.round(mc.player.distanceTo(p)) + "\u00a7f]");
+            if (getSetting(6).asToggle().state && ModuleManager.getModule(TotemPopCounter.class).isToggled())
+                info.add(" \u00a7fPopped [\u00a73" + (tpc.pops.containsKey(name) ? tpc.pops.get(name) : "0") + "\u00a7f]");
             for (String s : info) {
                 mc.textRenderer.drawWithShadow(e.matrix, s, (float) getSetting(2).asSlider().getValue(),
                         (float) getSetting(1).asSlider().getValue() + textHeight, 0xa0a0a0);
