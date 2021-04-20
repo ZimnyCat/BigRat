5f352161645be0129a4e285cc1a5ba2c4338580d
ZimnyCat <thezimnycat@gmail.com>
2021-04-20 19:24:40 +0500

    добавлены приколы с зачарами в PvPInfo

--- src/main/java/bleach/hack/module/mods/PvPInfo.java
+++ src/main/java/bleach/hack/module/mods/PvPInfo.java
@@ -7,11 +7,17 @@ import bleach.hack.setting.base.SettingSlider;
 import bleach.hack.setting.base.SettingToggle;
 import com.google.common.eventbus.Subscribe;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PvPInfo extends Module {
@@ -25,7 +31,9 @@ public class PvPInfo extends Module {
                 new SettingSlider("Width", 1, 1000, 150, 1), // 2
                 new SettingToggle("HP", true), // 3
                 new SettingToggle("Ping", true), // 4
-                new SettingToggle("Distance", true)); // 5
+                new SettingToggle("Distance", true), // 5
+                new SettingToggle("ProtectionSum", true), // 6
+                new SettingToggle("Sharpness", true)); // 7
     }
 
     @Subscribe
@@ -45,6 +53,8 @@ public class PvPInfo extends Module {
             if (getSetting(3).asToggle().state) info.add(" \u00a7fHP [\u00a73" + Math.round(p.getHealth() + p.getAbsorptionAmount()) + "\u00a7f]");
             if (getSetting(4).asToggle().state) info.add(" \u00a7fPing [\u00a73" + ping + "\u00a7f]");
             if (getSetting(5).asToggle().state) info.add(" \u00a7fDistance [\u00a73" + Math.round(mc.player.distanceTo(p)) + "\u00a7f]");
+            if (getSetting(6).asToggle().state) info.add(" \u00a7fProtSum [\u00a73" + protectionSum(p) + "\u00a7f]");
+            if (getSetting(7).asToggle().state) info.add(" \u00a7fSharpness [\u00a73" + sharpness(p) + "\u00a7f]");
             for (String s : info) {
                 mc.textRenderer.drawWithShadow(e.matrix, s, (float) getSetting(2).asSlider().getValue(),
                         (float) getSetting(1).asSlider().getValue() + textHeight, 0xa0a0a0);
@@ -52,5 +62,22 @@ public class PvPInfo extends Module {
             }
         }
         textHeight = 0;
+    }
+
+    private int protectionSum(PlayerEntity e) {
+        int sum = 0;
+        for (ItemStack item : e.getArmorItems()) {
+            for (Map.Entry<Enchantment, Integer> ench : EnchantmentHelper.get(item).entrySet()) {
+                if (ench.getKey() == Enchantments.PROTECTION) sum += ench.getValue();
+            }
+        }
+        return sum;
+    }
+
+    private int sharpness(PlayerEntity e) {
+        for (Map.Entry<Enchantment, Integer> ench : EnchantmentHelper.get(e.getMainHandStack()).entrySet()) {
+            if (ench.getKey() == Enchantments.SHARPNESS) return ench.getValue();
+        }
+        return 0;
     }
 }
