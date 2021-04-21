a2b3df6b356e2c66cc59033b9fc4dba262e564d8
ZimnyCat <thezimnycat@gmail.com>
2021-04-21 08:11:44 +0500

    типо оптимизация AutoRespond

--- src/main/java/bleach/hack/module/mods/AutoRespond.java
+++ src/main/java/bleach/hack/module/mods/AutoRespond.java
@@ -2,19 +2,27 @@ package bleach.hack.module.mods;
 
 import bleach.hack.BleachHack;
 import bleach.hack.event.events.EventReadPacket;
+import bleach.hack.event.events.EventTick;
 import bleach.hack.module.Category;
 import bleach.hack.module.Module;
+import bleach.hack.setting.base.SettingToggle;
 import bleach.hack.utils.BleachLogger;
 import bleach.hack.utils.file.BleachFileMang;
 import com.google.common.eventbus.Subscribe;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class AutoRespond extends Module {
+
+    List<String> gamers = new ArrayList<>();
+    List<String> msgs = new ArrayList<>();
+
     public AutoRespond() {
-        super("AutoRespond", KEY_UNBOUND, Category.CHAT, "Automatically responds to chat messages from selected player(s)");
+        super("AutoRespond", KEY_UNBOUND, Category.CHAT, "Automatically responds to chat messages from selected player(s)",
+                new SettingToggle("Reload", false));
     }
 
     public void init() {
@@ -22,14 +30,14 @@ public class AutoRespond extends Module {
         for (String s : arFiles) BleachFileMang.createFile("ar_" + s + ".txt");
         if (BleachFileMang.readFileLines("ar_offlineFormat.txt").isEmpty())
             BleachFileMang.appendFile("<%name%>", "ar_offlineFormat.txt");
+        gamers = BleachFileMang.readFileLines("ar_players.txt");
+        msgs = BleachFileMang.readFileLines("ar_messages.txt");
     }
     @Subscribe
     public void onChatMessage(EventReadPacket e) {
         MobOwner mo = new MobOwner();
         Random r = new Random();
         if (!(e.getPacket() instanceof GameMessageS2CPacket)) return;
-        List<String> gamers = BleachFileMang.readFileLines("ar_players.txt");
-        List<String> msgs = BleachFileMang.readFileLines("ar_messages.txt");
         String msg = ((GameMessageS2CPacket) e.getPacket()).getMessage().getString().toLowerCase();
         if (gamers.isEmpty()) disable("ar_players.txt");
         if (msgs.isEmpty()) disable("ar_messages.txt");
@@ -43,6 +51,17 @@ public class AutoRespond extends Module {
                 mc.player.sendChatMessage(msgs.get(r.nextInt(msgs.size())));
         }
     }
+
+    @Subscribe
+    public void onTick(EventTick e) {
+        if (getSetting(0).asToggle().state) {
+            gamers = BleachFileMang.readFileLines("ar_players.txt");
+            msgs = BleachFileMang.readFileLines("ar_messages.txt");
+            getSetting(0).asToggle().toggle();
+            BleachLogger.infoMessage("Files reloaded");
+        }
+    }
+
     private void disable(String fileName) {
         BleachLogger.errorMessage("./" + BleachHack.NAME + "/" + fileName +" is empty! Disabling AutoRespond...");
         BleachLogger.infoMessage("Check " + BleachHack.NAME + " in your Minecraft folder!");
