9751453c53d017342e1ce1932c4d0dbddf1e86be
ZimnyCat <thezimnycat@gmail.com>
2021-04-26 18:00:16 +0500

    добавлен killStreak в AutoEZ

--- src/main/java/bleach/hack/module/mods/AutoEZ.java
+++ src/main/java/bleach/hack/module/mods/AutoEZ.java
@@ -5,27 +5,35 @@ import bleach.hack.event.events.EventReadPacket;
 import bleach.hack.event.events.EventTick;
 import bleach.hack.module.Category;
 import bleach.hack.module.Module;
+import bleach.hack.module.ModuleManager;
+import bleach.hack.setting.base.SettingToggle;
 import bleach.hack.utils.BleachLogger;
 import bleach.hack.utils.file.BleachFileMang;
 import com.google.common.eventbus.Subscribe;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class AutoEZ extends Module {
 
     long killTime = 0;
+    List<String> lines = new ArrayList<>();
 
-    public AutoEZ() { super("AutoEZ", KEY_UNBOUND, Category.CHAT, "Does exactly what you think it does"); }
+    public AutoEZ() {
+        super("AutoEZ", KEY_UNBOUND, Category.CHAT, "Does exactly what you think it does",
+            new SettingToggle("Reload", false));
+    }
 
     @Override
     public void init() {
         super.init();
         if (!BleachFileMang.fileExists("AutoEZ.txt")) {
             BleachFileMang.createFile("AutoEZ.txt");
-            BleachFileMang.appendFile("EZ! " + BleachHack.CLIENT + " on top!", "AutoEZ.txt");
+            BleachFileMang.appendFile("EZ! " + BleachHack.CLIENT + " on top! %killStreak%", "AutoEZ.txt");
         }
+        lines = BleachFileMang.readFileLines("AutoEZ.txt");
     }
 
     @Override
@@ -50,16 +58,25 @@ public class AutoEZ extends Module {
 
     @Subscribe
     public void onTick(EventTick et) {
+        if (getSetting(0).asToggle().state) {
+            lines = BleachFileMang.readFileLines("AutoEZ.txt");
+            getSetting(0).asToggle().toggle();
+        }
+
         if (killTime != 0 && (System.currentTimeMillis() - killTime) > 200 && !mc.player.isDead()) {
             Random r = new Random();
-            List<String> lines = BleachFileMang.readFileLines("AutoEZ.txt");
-            if (lines.isEmpty()) mc.player.sendChatMessage("EZ! " + BleachHack.CLIENT + " on top!");
-            else mc.player.sendChatMessage(lines.get(r.nextInt(lines.size())));
+            if (lines.isEmpty()) mc.player.sendChatMessage("EZ! " + BleachHack.CLIENT + " on top! " + killStreak());
+            else mc.player.sendChatMessage(lines.get(r.nextInt(lines.size())).replace("%killStreak%", killStreak()));
             killTime = 0;
         }
 
         if (mc.player.isDead()) {
             killTime = 0;
         }
+    }
+
+    private String killStreak() {
+        KillStreak streak = (KillStreak) ModuleManager.getModule(KillStreak.class);
+        return streak.isToggled() ? "Kill streak: " + streak.kills : "";
     }
 }
