c298b7fe17c63255159165a2bbfcc47bf6dba3d0
ZimnyCat <thezimnycat@gmail.com>
2022-06-19 23:23:06 +0500

    переписан HoleTP

--- src/main/java/bleach/hack/module/mods/HoleTP.java
+++ src/main/java/bleach/hack/module/mods/HoleTP.java
@@ -4,29 +4,56 @@ import bleach.hack.event.events.EventTick;
 import bleach.hack.module.Category;
 import bleach.hack.module.Module;
 import com.google.common.eventbus.Subscribe;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.math.BlockPos;
 
-//insanely hard to code! mostly everything is stolen from bleach's step module
+import java.util.Arrays;
+import java.util.List;
+
 
 public class HoleTP extends Module {
-    public HoleTP() {
-        super("HoleTP", KEY_UNBOUND, Category.MOVEMENT, "AHH I FALL FAST");
-    }
 
-    private boolean flag;
+    BlockPos currentHole;
+
+    public HoleTP() { super("HoleTP", KEY_UNBOUND, Category.MOVEMENT, "Crystal PvP Holes HATE This Module..."); }
 
     @Subscribe
-    public void onTick(EventTick event)
-    {
-        if(mc.player == null || mc.world == null || mc.player.isInLava() || mc.player.isSubmergedInWater())
-        {
-            return;
+    public void onTick(EventTick event) {
+        BlockPos playerPos = mc.player.getBlockPos();
+
+        if (isHole(playerPos.down()) && !playerPos.down().equals(currentHole)) {
+            double playerX = Math.floor(mc.player.getX());
+            double playerZ = Math.floor(mc.player.getZ());
+            mc.player.updatePosition(playerX + 0.5, mc.player.getY() - 0.1, playerZ + 0.5);
+            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionOnly(playerX + 0.5, mc.player.getY() - 0.1, playerZ + 0.5, mc.player.isOnGround()));
+            currentHole = playerPos.down();
+            if (mc.player.stepHeight != 0) mc.player.stepHeight = 0;
         }
-        if(mc.player.isOnGround())
-        {
-            mc.player.setVelocity(mc.player.getVelocity().x, -1, mc.player.getVelocity().z);
-            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket(true));
-            flag = true;
+
+        if (!playerPos.equals(currentHole) && !playerPos.down().equals(currentHole)) {
+            mc.player.stepHeight = 0.5f;
+            currentHole = null;
         }
+    }
+
+    private boolean isHole(BlockPos pos) {
+        if (mc.world.getBlockState(pos).getBlock() != Blocks.AIR) return false;
+
+        List<BlockPos> poses = Arrays.asList(
+                pos.north(),
+                pos.east(),
+                pos.south(),
+                pos.west(),
+                pos.down()
+        );
+
+        for (BlockPos p : poses) {
+            Block b = mc.world.getBlockState(p).getBlock();
+            if (b != Blocks.OBSIDIAN && b != Blocks.BEDROCK) return false;
+        }
+
+        return true;
     }
 }
